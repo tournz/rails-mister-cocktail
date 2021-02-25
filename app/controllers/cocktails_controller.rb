f216e7634d4e34fa20dafe50e3f130ee9114d82b
Zacharie Tournant ztournant@icloud.com <ztournant@icloud.com>
2021-02-25 17:06:57 +0300

    FEAT: changed cocktail create action to redirect to newly created cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -13,7 +13,7 @@ class CocktailsController < ApplicationController
   def create
     @cocktail = Cocktail.new(params_cocktail)
     if @cocktail.save
-      redirect_to cocktails_path
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
